483715c34dfd155e0160d621f3149971c632b887
antkss <anfixgapps@gmail.com>
2025-01-06 15:14:13 +0700

    build gdb 1

--- a.sh
+++ a.sh
@@ -4,8 +4,8 @@ pacman-key --populate
 pacman -Sy
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 cp ./pacman.conf /etc
 
 pacman -Syuu git wget 7zip base-devel source-highlight --noconfirm
