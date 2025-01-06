86d56454a89dd7ac527c0ff5ebb438d064f0c352
antkss <anfixgapps@gmail.com>
2025-01-06 15:08:36 +0700

    build gdb 1

--- a.sh
+++ a.sh
@@ -3,7 +3,7 @@ pacman -Syuu git wget --noconfirm
 wget "https://github.com/Jguer/yay/releases/download/v12.4.2/yay_12.4.2_x86_64.tar.gz"
 tar -xf yay_12.4.2_x86_64.tar.gz
 ln -s $(pwd)/yay_12.4.2_x86_64/yay /bin/yay
-yay -Syuu 7zip base-devel source-highlight pod2man --noconfirm
+yay -Syuu 7zip base-devel source-highlight --noconfirm
 git clone https://github.com/antkss/gdb-fzf
 cd gdb-fzf
 ./makepkg
