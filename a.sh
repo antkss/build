f9545d92e7a80ba011ed7a92ac486002bc798d37
antkss <anfixgapps@gmail.com>
2025-01-06 14:13:47 +0700

    update

--- a.sh
+++ a.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 pacman -Syuu git 7zip --noconfirm
-
-# git clone https://github.com/antkss/gdb-fzf
-# cd gdb-fzf
-# ./makepkg
+git clone https://github.com/antkss/gdb-fzf
+cd gdb-fzf
+./makepkg
 mkdir /output
-# mv *.pkg.tar* /output
+mv *.pkg.tar* /output
 cd /output
 echo "file path" > filepath
 7z a output.zip
