967c6df233e2cd5706f5bc5942eabadf4565cad0
antkss <anfixgapps@gmail.com>
2025-01-06 15:13:00 +0700

    build gdb 1

--- a.sh
+++ a.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
-pacman -Syuu git wget --noconfirm
-wget "https://github.com/Jguer/yay/releases/download/v12.4.2/yay_12.4.2_x86_64.tar.gz"
-tar -xf yay_12.4.2_x86_64.tar.gz
-ln -s $(pwd)/yay_12.4.2_x86_64/yay /bin/yay
-yay -Syuu 7zip base-devel source-highlight --noconfirm
+pacman-key --init
+pacman-key --populate
+pacman -Sy
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+cp ./pacman.conf /etc
+
+pacman -Syuu git wget 7zip base-devel source-highlight --noconfirm
+
 git clone https://github.com/antkss/gdb-fzf
 cd gdb-fzf
 ./makepkg
