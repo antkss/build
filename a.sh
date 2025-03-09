430403b7dee34140370adb6580d109ae9c036bdb
lmaodark <lmao@gmail.com>
2025-03-09 22:21:15 +0700

    update

--- a.sh
+++ a.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/bash
-pacman-key --init
-pacman-key --populate
-pacman -Sy
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-cp ./pacman.conf /etc
+# pacman-key --init
+# pacman-key --populate
+# pacman -Sy
+# pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+# pacman-key --lsign-key 3056513887B78AEB
+# pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+# pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+# cp ./pacman.conf /etc
+#
+# pacman -Syuu git wget 7zip base-devel source-highlight pod2man --noconfirm
+#
+# git clone https://github.com/antkss/gdb-fzf
+# cd gdb-fzf
+# ./makepkg
+# mkdir /output
+# mv *.pkg.tar* /output
+# cd /output
+# echo "file path" > filepath
+# 7z a output.zip
+# mv output.zip /
 
-pacman -Syuu git wget 7zip base-devel source-highlight pod2man --noconfirm
-
-git clone https://github.com/antkss/gdb-fzf
-cd gdb-fzf
-./makepkg
-mkdir /output
-mv *.pkg.tar* /output
-cd /output
-echo "file path" > filepath
-7z a output.zip
-mv output.zip /
+apt install git python3 wget build-essential -y
+wget "https://raw.githubusercontent.com/antkss/sub/refs/heads/master/a.sh"
+bash a.sh
 
